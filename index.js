e231a7a0eaafb519572deaadac54412485fd659c
Karabo Mjiyako <149914891+KarabooMjiyako@users.noreply.github.com>
2024-01-01 19:57:57 +0200

    Update index.js\

--- index.js
+++ index.js
@@ -5,19 +5,29 @@ function Book(title, author, isbn) {
     this.isbn = isbn;
 }
 
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const isbnInput = document.querySelector("#ISBN");
+const button = document.querySelector(".btn");
+const bookListElement = document.querySelector("#book-list");
+
+console.log("titleInput:", titleInput);
+console.log("authorInput:", authorInput);
+console.log("isbnInput:", isbnInput);
+console.log("button:", button);
+
 // BookList constructor
 function BookList() {
     this.books = [];
 
-    // Method to add a book to the list
     this.addBook = function(book) {
         this.books.push(book);
+        this.displayBooks();
     };
 
-    // Method to display the books in the book list
     this.displayBooks = function() {
         const bookList = document.querySelector("#book-list");
-        bookList.innerHTML = ""; // Clear previous content
+        bookList.innerHTML = "";
 
         this.books.forEach(function(book) {
             const bookListRow = document.createElement("tr");
@@ -39,22 +49,17 @@ function BookList() {
     };
 }
 
-// Create instances of Book and BookList
-const titleInput = document.querySelector("#title");
-const authorInput = document.querySelector("#author");
-const isbnInput = document.querySelector("#ISBN");
-const button = document.querySelector(".btn");
-
+// Instances of Book and BookList
 const bookList = new BookList();
 
 button.addEventListener("click", function() {
-    if (titleInput.value === "" && authorInput.value === "" && isbnInput.value === "") {
-        alert("Enter any input");
+    console.log("Button clicked");
+    if (titleInput.value == "" && authorInput.value == "" && isbnInput.value == "") {
+        alert("Enter any book");
     } else {
         const newBook = new Book(titleInput.value, authorInput.value, isbnInput.value);
+        console.log("New Book:", newBook);
         bookList.addBook(newBook);
-        bookList.displayBooks();
     }
 });
-})
 
